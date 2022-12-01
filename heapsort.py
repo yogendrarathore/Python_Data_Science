a576a07e2301861f251e4aa1f668176eef9ce3df
yogendrarathore <109740933+yogendrarathore@users.noreply.github.com>
2022-12-01 13:52:27 +0530

    Add files via upload
    heap sort

---
+++ heapsort.py
@@ -0,0 +1,46 @@
+# Heap Sort in python
+
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+  
+    if l < n and arr[i] < arr[l]:
+        largest = l
+  
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+  
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+  
+  
+def heapSort(arr):
+    n = len(arr)
+  
+      # Build max heap
+    for i in range(n//2, -1, -1):
+        heapify(arr, n, i)
+  
+    for i in range(n-1, 0, -1):
+          # Swap
+        arr[i], arr[0] = arr[0], arr[i]
+  
+          # Heapify root element
+        heapify(arr, i, 0)
+  
+  
+arr = []
+n=int(input("enter nuber of terms:"))
+for i in range(n):
+    ele=int(input("enter elements:"))
+    arr.append(ele)
+  
+heapSort(arr)
+n = len(arr)
+print("Sorted array is")
+for i in range(n):
+    print(arr[i])
+  
